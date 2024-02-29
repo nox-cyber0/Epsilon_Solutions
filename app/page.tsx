211b0ed661c0ec5f512330913b4791c439be8c00
nox-cyber0 <mhabubakar999@outlook.com>
2024-02-29 17:54:44 +0000

    Navigation menu and home page mobile state UI updates

--- app/page.tsx
+++ app/page.tsx
@@ -2,19 +2,19 @@ import Link from 'next/link';
 
 export default function Home() {
 	return (
-		<main className='min-h-screen'>
-			<div className='w-full flex'>
-				<section className='w-2/3 flex bg-slate-50 p-12 md:pl-20 h-screen items-center justify-start'>
+		<main className='min-h-screen pt-16'>
+			<div className='w-full md:flex'>
+				<section className='w-full md:w-2/3 block md:flex bg-slate-50 p-12 md:px-24 md:h-screen items-center justify-start'>
 					<div className='block'>
-						<h4 className='block w-full text-6xl font-bold text-left text-slate-700 capitalize'>
+						<h4 className='block w-full text-4xl md:text-6xl font-bold text-center md:text-left text-slate-700 capitalize'>
 							Epsilon InfoTech Solutions
 						</h4>
-						<p className='block w-full text-base font-semibold mt-3 text-slate-600 capitalize'>
+						<p className='block w-full text-sm md:text-base text-center md:text-left font-semibold mt-3 text-slate-600 capitalize'>
 							Delivering Creative Solutions for Your Business Growth
 						</p>
 					</div>
 				</section>
-				<section className='w-1/3 flex bg-blue-700 h-screen'></section>
+				<section className='w-full md:w-1/3 block md:flex bg-blue-700 h-64 md:h-screen'></section>
 			</div>
 		</main>
 	);
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -25,54 +25,64 @@ export default function Navbar() {
 						/>
 					</Link>
 					<Sheet>
-						<SheetTrigger className='block w-full text-right font-mono text-sm font-semibold'>
-							Menu
+						<SheetTrigger>
+							<span className='block w-full text-right font-mono text-sm font-semibold'>
+								Menu
+							</span>
 						</SheetTrigger>
 						<SheetContent>
-							<SheetHeader>
-								<SheetTitle>
-									<Image
-										src={logo}
-										alt='Epsilon logo'
-										width={60}
-										height={60}
-									/>
-								</SheetTitle>
-								<SheetDescription>
-									<div className='block mt-6 text-left'>
-										<Link
-											className='text-slate-400 text-base font-semibold hover:text-slate-800 transition block mb-3'
-											href='/'
-										>
-											Home
-										</Link>
-										<Link
-											className='text-slate-400 text-base font-semibold hover:text-slate-800 transition block mb-3'
-											href='/services'
-										>
-											Services
-										</Link>
-										<Link
-											className='text-slate-400 text-base font-semibold hover:text-slate-800 transition block mb-3'
-											href='/pricing'
-										>
-											Pricing
-										</Link>
-										<Link
-											className='text-slate-400 text-base font-semibold hover:text-slate-800 transition block mb-3'
-											href='/portfolio'
-										>
-											Portfolio
-										</Link>
-										<Link
-											className='text-slate-400 text-base font-semibold hover:text-slate-800 transition block mb-3'
-											href='/about-us'
-										>
-											About Us
-										</Link>
-									</div>
-								</SheetDescription>
-							</SheetHeader>
+							<h3>
+								<Image
+									src={logo}
+									alt='Epsilon logo'
+									width={50}
+									height={50}
+								/>
+							</h3>
+							<div className='block mt-6 text-left'>
+								<hr className='text-slate-400' />
+								<Link
+									className='text-slate-400 text-base font-semibold hover:text-slate-800 transition block my-3'
+									href='/'
+								>
+									Home
+								</Link>
+								<hr className='text-slate-400' />
+								<Link
+									className='text-slate-400 text-base font-semibold hover:text-slate-800 transition block my-3'
+									href='/services'
+								>
+									Services
+								</Link>
+								<hr className='text-slate-400' />
+								<Link
+									className='text-slate-400 text-base font-semibold hover:text-slate-800 transition block my-3'
+									href='/portfolio'
+								>
+									Portfolio
+								</Link>
+								<hr className='text-slate-400' />
+								<Link
+									className='text-slate-400 text-base font-semibold hover:text-slate-800 transition block my-3'
+									href='/about-us'
+								>
+									About Us
+								</Link>
+								<hr className='text-slate-400' />
+								<Link
+									className='text-slate-400 text-base font-semibold hover:text-slate-800 transition block my-3'
+									href='/contact'
+								>
+									Contact Us
+								</Link>
+								<hr className='text-slate-400' />
+								<Link
+									href='/'
+									className='bg-blue-700 text-white hover:bg-blue-800 rounded-md p-2.5 text-xs font-semibold uppercase transition w-full block text-center my-3'
+								>
+									Get a quote
+								</Link>
+							</div>
 						</SheetContent>
 					</Sheet>
 				</div>
@@ -99,22 +109,28 @@ export default function Navbar() {
 							Services
 						</Link>
 						<Link
-							href='/pricing'
-							className='text-sm font-semibold text-slate-400 hover:text-slate-800 transition'
-						>
-							Pricing
-						</Link>
-						<Link
 							href='/portfolio'
 							className='text-sm font-semibold text-slate-400 hover:text-slate-800 transition'
 						>
 							Portfolio
 						</Link>
 						<Link
+							href='/pricing'
+							className='text-sm font-semibold text-slate-400 hover:text-slate-800 transition'
+						>
+							Pricing
+						</Link>
+						<Link
 							href='/about-us'
 							className='text-sm font-semibold text-slate-400 hover:text-slate-800 transition'
 						>
 							About Us
+						</Link>
+						<Link
+							href='/contact'
+							className='text-sm font-semibold text-slate-400 hover:text-slate-800 transition'
+						>
+							Contact Us
 						</Link>
 					</div>
 					<Link
